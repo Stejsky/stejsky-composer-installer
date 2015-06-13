2e0801dd6191e89f1ed0d50cb754640262be173d
Stejsky <jakub@stejsky.com>
2015-06-13 18:13:56 +0200

    initial commit

---
+++ NetteInstaller.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ *  Basic Composer Installer for Nette.
+ *  @author Jakub Stejskal, jakub@stejsky.com
+ *  @version 1.0
+ */
+
+namespace Composer\Installers;
+
+
+class NetteInstaller {
+
+} 
